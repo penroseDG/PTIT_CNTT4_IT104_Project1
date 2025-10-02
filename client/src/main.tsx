32d9da412fa6f8a0ab912dde1f149bcdec31dbf5
penroseDG <dudangdaide@gmail.com>
2025-10-02 20:57:12 +0700

    update

---
+++ client/src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import routers from './routers/router.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={routers}></RouterProvider>
+  </StrictMode>,
+)
---
+++ client/src/routers/router.tsx
@@ -0,0 +1,12 @@
+import { App } from "antd";
+import { createBrowserRouter } from "react-router-dom";
+
+const routers = createBrowserRouter([
+    {
+        path : "/",
+        element : <App></App>
+    },
+    
+])
+
+export default routers ; 
